4a3dfe3eb98b01a6e34b141876d4d3449da1c3ab
Katie Silverio <astrosilverio@gmail.com>
2015-02-26 21:43:54 -0400

    you got that james dean daydream look in your eye (STYLE)

--- hogwartsexceptions.py
+++ hogwartsexceptions.py
@@ -1,14 +1,17 @@
 class MaraudersMapError(Exception):
-	'''Should hopefully not get many of these, can't figure out where I'd need them'''
+	""" Should hopefully not get many of these, can't figure out where I'd need them.
+    """
 	pass
 
 class RowlingError(Exception):
-	''' These should be if user is trying to do something they can't,
-        like go north from a room with one path to the south'''
+	""" These should be if user is trying to do something they can't,
+        like go north from a room with one path to the south.
+    """
 	pass
 
 class LegilimensError(Exception):
-	'''These are for weird commands I don't understand'''
+	""" These are for weird input I don't understand.
+    """
 	pass
 
 class Messages(object):
